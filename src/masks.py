2d3da2d72385ba89bce8398ea1ca761922b70b23
Mutikova.Ya <mutikovaana@gmail.com>
2025-03-25 10:30:16 +0300

    init commit

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,24 @@
 def get_mask_card_number(number: str):
-    first = number[:4]
-    second = number[4:6]
+    first = number[:13]
+    second = number[14:18]
+    third = number[18:20]
     last = number[-4:]
-    return f"{first} {second}** **** {last}"
+    """Срезаем числа"""
+    return f"{first} {second} {third}** **** {last}"
 
 
 def get_mask_account(number: str):
     once = number[-4:]
     return f"**{once}"
+    """Возвращаем замаскированное число"""
+
+
+def get_date(date: str):
+    day = date[8:10]
+    month = date[5:7]
+    year = date[:4]
+    res = f"{day}.{month}.{year}"
+    return res
+    """выводим дату"""
+
+
--- src/widget.py
+++ src/widget.py
@@ -2,21 +2,23 @@ from calendar import month
 from datetime import datetime
 
 
-def mask_account_card(number: str):
-    if "Счет" in number:
-        first = number[:4]
-        last = number[-3:]
-        return f"{first} **** **** *{last}"
-    else:
-        first_one = number[:2]
-        second_two = number[4:6]
-        last_three = number[-3:]
-        return f"{first_one}** {second_two}** **** *{last_three}"
+def get_mask_card_number(number: str):
+    first = number[:13]
+    second = number[14:18]
+    third = number[18:20]
+    last = number[-4:]
+    """Срезаем числа"""
+    return f"{first} {second} {third}** **** {last}"
 
+
+def get_mask_account(number: str):
+    once = number[-4:]
+    return f"**{once}"
+    """Возвращаем замаскированное число"""
 
 def get_date(date: str):
-    day = date[9:10]
-    month = date[6:7]
+    day = date[8:10]
+    month = date[5:7]
     year = date[:4]
     res = f"{day}.{month}.{year}"
     return res
