769f06e8ab197cc95f29c922a4ab47ed948e5c6c
Mutikova.Ya <mutikovaana@gmail.com>
2025-03-31 15:17:43 +0300

    внесены правки в домашнее задание и создан файл README создан файл tests

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,26 @@
+from src.masks import get_mask_card_number, get_mask_account
 
-from src.masks import get_mask_card_number, get_mask_account, number
 
-if "Счет" in number:
-    result = get_mask_account() # реализован в masks
-else:
-    result = get_mask_card_number()  # реализован в masks
+def mask_account_card(card_number):
+    """Определяем является ли счетом"""
+    card_numbers = ''
+    card_name = ''
+    for i in card_number:
+        if i.isdigit():
+            card_numbers += i
+        else:
+            card_name += i
+    if "Счет" in card_number:
+        result = get_mask_account(card_numbers)
+    else:
+        result = get_mask_card_number(card_numbers)
+    return f"{card_name} {result}"
 
+
+def get_date(date: str):
+    """выводим дату"""
+    day = date[8:10]
+    month = date[5:7]
+    year = date[:4]
+    res = f"{day}.{month}.{year}"
+    return res
