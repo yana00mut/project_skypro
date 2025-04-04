43987b7428ea4c7ec8e1448f1e494fa6bc42647a
Mutikova.Ya <mutikovaana@gmail.com>
2025-04-04 15:34:03 +0300

    создана папка tests и модули test_masks.py и test_processing.py

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,27 @@
+from calendar import month
+from datetime import datetime
 
-from src.masks import get_mask_card_number, get_mask_account, number
 
-if "Счет" in number:
-    result = get_mask_account() # реализован в masks
-else:
-    result = get_mask_card_number()  # реализован в masks
+def get_mask_card_number(number: str):
+    """Срезаем числа"""
+    first = number[:13]
+    second = number[14:18]
+    third = number[18:20]
+    last = number[-4:]
+    """Срезаем числа"""
+    return f"{first} {second} {third}** **** {last}"
 
+
+def get_mask_account(number: str):
+    """Возвращаем замаскированное число"""
+    once = number[-4:]
+    return f"**{once}"
+
+
+def get_date(date: str):
+    """"Выводим дату"""
+    day = date[8:10]
+    month = date[5:7]
+    year = date[:4]
+    res = f"{day}.{month}.{year}"
+    return res
