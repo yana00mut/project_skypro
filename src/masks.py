953e08ac731208538f8db654a9bc769016495804
Mutikova.Ya <mutikovaana@gmail.com>
2025-04-25 13:40:41 +0300

    Написаны логи для модуля masks

--- src/masks.py
+++ src/masks.py
@@ -1,25 +1,46 @@
-number = input()
+import logging
+import os
+
+os.makedirs("logs", exist_ok=True)
+
+logging.basicConfig(
+    filename='logs/utils.log',
+    filemode='w',
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    encoding='utf-8'
+)
+
+logger = logging.getLogger(__name__)
 
 
 def get_mask_card_number(number: str):
+    """Срезаем числа"""
+    logger.info(f"Вызвана get_mask_card_number с аргументом: {number}")
     first = number[:13]
     second = number[14:18]
     third = number[18:20]
     last = number[-4:]
-    """Срезаем числа"""
-    return f"{first} {second} {third}** **** {last}"
+    result = f"{first} {second} {third}** **** {last}"
+    logger.info(f"Результат: {result}")
+    return result
 
 
 def get_mask_account(number: str):
-    once = number[-4:]
-    return f"**{once}"
     """Возвращаем замаскированное число"""
+    logger.info(f"Вызвана get_mask_account с аргументом: {number}")
+    result = f"**{number[-4:]}"
+    logger.info(f"Результат: {result}")
+    return result
 
 
 def get_date(date: str):
+    """Выводим дату"""
+    logger.info(f"Вызвана get_date с аргументом: {date}")
     day = date[8:10]
     month = date[5:7]
     year = date[:4]
-    res = f"{day}.{month}.{year}"
-    return res
-    """выводим дату"""
+    result = f"{day}.{month}.{year}"
+    logger.info(f"Результат: {result}")
+    return result
+
